e1f05d5c6f9bea734a19d640f1a9289ef95ab645
valeryaosta <gerasimenko.valery@gmail.com>
2021-08-26 15:42:43 +0300

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Lesson1 from './lessons/lesson1/lesson1';
+
+
+function App() {
+    return (
+        <div className="container">
+            <Lesson1 />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/lessons/lesson1/lesson1.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import CurrencyExchangeContainer from '../../containers/CurrencyExchangeContainer/CurrencyEContainer'
+
+
+
+const Lesson1: React.FunctionComponent = () => {
+    return (
+    <React.Fragment>
+        <CurrencyExchangeContainer />
+    </React.Fragment>
+  );
+};
+
+export default Lesson1;
+
